3d26e4aaf0ab1a70a5c2894edb40233c5a67e64a
Mukhil <mukhiljjsa@gmail.com>
2026-03-14 14:01:16 +0530

    make category and user optional

--- expenses/models.py
+++ expenses/models.py
@@ -14,8 +14,8 @@ class Expense(models.Model):
     title = models.CharField(max_length=255)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     date = models.DateField()
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
